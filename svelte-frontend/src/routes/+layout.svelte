43e038834d6d0653d125f95cff1606293ef19e06
Slipstream <faxcluster20@protonmail.com>
2025-05-12 19:19:44 -0600

    Integrate svelte-frontend into main repository

---
+++ svelte-frontend/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+    import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+</script>
+
+<div class="p-4">
+    <ThemeToggle />
+</div>
+
+<slot />
---
+++ svelte-frontend/tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+import skeleton from '@skeletonlabs/tw-plugin';
+import typography from '@tailwindcss/typography';
+
+export default {
+content: ['./src/**/*.{html,js,svelte,ts}'],
+
+theme: {
+extend: {}
+},
+presets: [skeleton],
+
+plugins: [typography, skeleton]
+} as Config;
